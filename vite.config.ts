34ebc933ac33c62efc4ff463e40cd6d073f689d9
takuma <>
2024-07-10 15:48:57 +0900

    Chore: テスト設定ファイルを追加

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest" />
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsConfigPaths from 'vite-tsconfig-paths'
@@ -5,4 +6,9 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsConfigPaths()],
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: ["./vitest-setup.ts"]
+  }
 })
